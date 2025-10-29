ae33c8165e47101e842d98b9ca221de9fa714668
Matthew F <mfen130@comcast.net>
2025-10-29 16:28:20 -0400

    Fix import issue for Render deployment

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes.companies import router as companies_router
+from routes.companies import router as companies_router
 from dotenv import load_dotenv
 import os
 load_dotenv()
